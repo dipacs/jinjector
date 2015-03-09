bf07e5e7c2ec0358e7e996dcf1447b0b1db0511c
Ipacs Dávid <david.ipacs@doctusoft.com>
2015-03-09 12:38:13 +0100

    JavaDoc

--- src/main/java/com/eagerlogic/jinjector/shared/JInjector.java
+++ src/main/java/com/eagerlogic/jinjector/shared/JInjector.java
@@ -17,6 +17,14 @@ public class JInjector {
 	private static final Map<String, JInjector> namedInjectors = new HashMap<>();
 	private static final Map<String, JInjector> packageInjectors = new HashMap<>();
 
+	/**
+	 * Creates a named injector. Named injectors can accessed by it's registered names.
+	 * If an injector is already defined whith the given name than this method throws an exception.
+	 *
+	 * @param name The name of the injector which can be used to later retrieve it.
+	 *
+	 * @return The new injector which is associated with the given name.
+	 */
 	public static JInjector createNamed(String name) {
 		if (name == null) {
 			throw new NullPointerException("The 'name' parameter can not be null.");
@@ -30,6 +38,14 @@ public class JInjector {
 		return injector;
 	}
 
+	/**
+	 * Returns a named injector which is associated with the given name. If no injector can be found than this
+	 * method is throws an exception.
+	 *
+	 * @param name The name of the injector.
+	 *
+	 * @return The injector which is associated with the given name.
+	 */
 	public static JInjector getInjector(String name) {
 		JInjector res = namedInjectors.get(name);
 		if (res == null) {
@@ -38,6 +54,14 @@ public class JInjector {
 		return res;
 	}
 
+	/**
+	 * Creates a package injector associated with the given package name.
+	 * If an injector is already defined for the given package than this method throws an exception.
+	 *
+	 * @param packageName The name of the package.
+	 *
+	 * @return The new injector.
+	 */
 	public static JInjector createPackageInjector(String packageName) {
 		if (packageName == null) {
 			throw new NullPointerException("The 'name' parameter can not be null.");
@@ -51,6 +75,12 @@ public class JInjector {
 		return injector;
 	}
 
+	/**
+	 * Returns the injector which is associated with the caller class's package, or one of it's parent package.
+	 * If no injector can be found than this method throws an exception.
+	 *
+	 * @return The injector which is associated with the caller class's package, or one of it's parent package.
+	 */
 	public static JInjector getPackageInjector() {
 		StackTraceElement[] st = Thread.currentThread().getStackTrace();
 		StackTraceElement prevElement = st[2];
@@ -70,6 +100,14 @@ public class JInjector {
     private final Map<Class<?>, Object> instances = new HashMap<>();
     private final Map<Class<?>, Provider<?>> providers = new HashMap<>();
 
+	/**
+	 * Returns the instance which is associated with the given type.
+	 *
+	 * @param <T> The type of the instance which you want to get.
+	 * @param type The type of the instance which you want to get.
+	 *
+	 * @return The instance which is associated with the given type.
+	 */
     public <T> T get(Class<T> type) {
         T res = (T) instances.get(type);
         if (res == null) {
@@ -82,6 +120,14 @@ public class JInjector {
         return res;
     }
 
+	/**
+	 * Returns the provider which is associated with the given type.
+	 *
+	 * @param <T> The type of the instance which you want to get.
+	 * @param type The type of the instance which you want to get.
+	 *
+	 * @return The provider which is associated with the given type.
+	 */
     public <T> Provider<T> getProvider(final Class<T> type) {
         return new Provider<T>() {
 
@@ -100,6 +146,13 @@ public class JInjector {
         };
     }
 
+	/**
+	 * Registers an instance with a given type. Use this method to create a singleton binding.
+	 *
+	 * @param <T> The type of the registered instance.
+	 * @param type The type of the registered instance. This can be used to retrieve the instance later.
+	 * @param instance The instance which will registered for the given type.
+	 */
     public <T> void register(Class<T> type, T instance) {
         if (type == null) {
             throw new NullPointerException("The 'type' parameter can not be null.");
@@ -111,6 +164,14 @@ public class JInjector {
         instances.put(type, instance);
     }
 
+	/**
+	 * Registers an provider with a given type which will be called every time when an instance of the given type is requested.
+	 * Use this method to create a dynamic bind.
+	 *
+	 * @param <T> The type of the registered provider.
+	 * @param type The type of the registered provider. This can be used to retrieve the provider later.
+	 * @param provider The provider which will registered for the given type.
+	 */
     public <T> void register(Class<T> type, Provider<T> provider) {
         if (type == null) {
             throw new NullPointerException("The 'type' parameter can not be null.");
--- src/main/java/com/eagerlogic/jinjector/shared/Provider.java
+++ src/main/java/com/eagerlogic/jinjector/shared/Provider.java
@@ -6,11 +6,18 @@
 package com.eagerlogic.jinjector.shared;
 
 /**
+ * This interface is responsible to provide an instance of a class. You can think it as a factory.
  *
  * @author dipacs
+ * @param <T> The type of the provided instance.
  */
 public interface Provider<T> {
-    
+
+	/**
+	 * Returns an instance (new or cached) of T.
+	 *
+	 * @return
+	 */
     public T get();
-    
+
 }
